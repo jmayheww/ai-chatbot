59bf705d30250b9d017fa441805c708d21f0e9e8
Joshua <joshsmayhew@gmail.com>
2023-11-04 13:22:29 +0700

    create migration for  action logs table in db

--- database/migrations/2023_11_04_060014_create_action_logs_table.php
+++ database/migrations/2023_11_04_060014_create_action_logs_table.php
@@ -15,8 +15,13 @@ class CreateActionLogsTable extends Migration
     {
         Schema::create('action_logs', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
+            $table->foreignId('chat_session_id')->constrained()->onDelete('cascade');
+            $table->string('action'); // Describes the action performed
+            $table->json('details')->nullable(); // Could store additional details about the action in JSON format
             $table->timestamps();
         });
+
     }
 
     /**
